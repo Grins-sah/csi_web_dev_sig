4abdd0cfbc5929256bd519e0bad8ebd494bc0173
Grins sah <grinssah9@gmail.com>
2026-03-31 15:36:43 +0530

    added logic of the user creation and unpdate

--- index.mjs
+++ index.mjs
@@ -1,18 +1,26 @@
-import express from 'express'   // import of the express object fromy the express module .mjs
-// const express = require('express) .js
-const app = express();      // this opens a tcp socket with the os
-import cors from 'cors'
-app.use(express.json())
-app.use(cors());
-
-app.get('/',(req,res)=>{
-    // it will talk with the db
+// we have to make a https backend
+import express from 'express' // import the https library
+const app = express();
+app.use(express.json()); // 
+app.get('/',(req,res)=>{ // lambda function 
     res.json({
-        msg:"hello csi members"
+        "msg":"hello"
     })
 })
-app.listen(5000,()=>{
-    console.log("backend is running on port 5000");
+const a = [];
+app.post('/user',(req,res)=>{
+    console.log(req.body);
+    a.push(req.body);
+    res.json({
+        msg:"user added"
+    })
+})
+app.get('/user',(req,res)=>{
+    res.json({
+        "user":a
+    })
+})
+app.listen(3000,()=>{
+    console.log("backend is running on port 3000")
 });
-
 
