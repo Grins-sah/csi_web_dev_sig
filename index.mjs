b71311b2262f3731a674f606a51f6d75b57cf795
Grins sah <grinssah9@gmail.com>
2026-03-31 16:10:26 +0530

    added login and signup

--- index.mjs
+++ index.mjs
@@ -7,18 +7,51 @@ app.get('/',(req,res)=>{ // lambda function
         "msg":"hello"
     })
 })
-const a = [];
+const user = [];
 app.post('/user',(req,res)=>{
     console.log(req.body);
-    a.push(req.body);
+    user.push(req.body);
     res.json({
         msg:"user added"
     })
 })
 app.get('/user',(req,res)=>{
     res.json({
-        "user":a
+        "user":user
     })
+})
+app.post('/signup',(req,res)=>{
+    const user_name = req.body.name;
+    const user_password = req.body.password;
+    user.push({user:user_name,password:user_password});
+    // does this user exist or not
+    console.log(`user created with user name ${user_name} `)
+    res.status(201).send()
+
+})
+app.post('/login',(req,res)=>{
+    const user_name = req.body.name;
+    const user_password = req.body.password;
+    let flag = false;
+    user.forEach((v)=>{
+        console.log(v);
+        if(v.user ===user_name && v.password ===user_password){
+            res.json({
+                user:user_name,
+                msg:"sign in"
+            })
+            flag = true;
+            return;
+        }
+    })
+    if(!flag){
+        res.status(404).json({
+            user:user_name,
+            msg:"not found"
+        })
+    }
+
+
 })
 app.listen(3000,()=>{
     console.log("backend is running on port 3000")
