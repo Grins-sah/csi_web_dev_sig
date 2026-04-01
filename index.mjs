22c4f821c2beae7ebef5f80993def4e67e614be8
Grins sah <grinssah9@gmail.com>
2026-04-01 21:41:38 +0530

    added the url

--- index.mjs
+++ index.mjs
@@ -1,8 +1,22 @@
 // we have to make a https backend
 import express from 'express' // import the https library
 import bcrypt from 'bcrypt'
-
+import mongoose from 'mongoose'; 
+async function connection(){
+    await mongoose.connect("/csi")
+    console.log("mongoose connected");
+}
 const app = express();
+connection();
+const UserSchema = new mongoose.Schema({
+    name:String,
+    password:String,
+    div:Number
+}) 
+
+const User = mongoose.model('User',UserSchema);
+
+
 app.use(express.json()); // 
 app.get('/',(req,res)=>{ // lambda function 
     res.json({
@@ -22,19 +36,36 @@ app.get('/user',(req,res)=>{
         "user":user
     })
 })
-app.post('/signup', (req, res) => {
+app.post('/signup',async (req, res) => {
     const user_name = req.body.name;
     const user_password = req.body.password;
-    const existingUser = user.find((u) => u.user === user_name);
-    if (existingUser) {
-        return res.status(409).json({
-            msg: "User already exists"
-        });
+    const user_div = req.body.div;
+    const hashed_password = bcrypt.hashSync(user_password,5);
+    const data = await User.create({
+        name:user_name,
+        password:hashed_password,
+        div:user_div
+    });
+    // const existingUser = user.find((u) => u.user === user_name);
+    // if (existingUser) {
+    //     return res.status(409).json({
+    //         msg: "User already exists"
+    //     });
         
-    }
-    user.push({user:user_name,password:user_password});
-    console.log(`user created with user name ${user_name} `)
-    res.status(201).send()
+    // }
+    // user.push({user:user_name,password:user_password});
+    console.log(`user created with user name ${user_name} with the user id ${data._id} `)
+    res.status(201).send("user created")
+
+})
+
+app.post('/update',async (req,res)=>{
+    const id = req.body.id;
+    const data = await User.updateOne({_id:id},{
+        name:"grins sah"
+    });
+
+    return res.send("updated");
 
 })
 
@@ -43,9 +74,9 @@ app.post('/login',async (req,res)=>{
     const user_password = req.body.password;
     let flag = false;
     for(let i = 0;i<(user.length);i++){
-        const hashed_password =  bcrypt.hashSync(user[i].password,5);
+        const hashed_password =  bcrypt.hashSync(user[i].password,1);
         console.log(`password ${user[i].password}  sha256 hash :${ hashed_password }`);
-        // bcrypt.compare()
+        // bcrypt.compare()p
         if(user[i].user ===user_name && user[i].password ===user_password){
             return res.json({
                 user:user_name,
@@ -63,6 +94,28 @@ app.post('/login',async (req,res)=>{
     }
 
 
+})
+
+/*
+promise is noting but a asycn call
+print("hello")
+// async code => is still running 
+print("next")
+
+*/
+
+app.post('/validate', (req,res)=>{
+    const passwordHased = req.body.hashed;
+    const password = req.body.password;
+    //
+    bcrypt.compare(password,passwordHased).then((flag)=>{
+        console.log(flag);
+        if(flag){
+            return res.send("password match")
+        }else{
+            return res.send("password not matched");
+        }
+    })
 })
 app.listen(3000,()=>{
     console.log("backend is running on port 3000")
